87e5b2cfe57172bc665c83b31763fb1e3950f2a1
Isaiah Omame <112702026+OmameIsaiah@users.noreply.github.com>
2024-11-05 23:29:56 +0100

    Add files via upload

---
+++ src/main/java/com/elastic/search/elastic_search_crud/ElasticSearchCrudApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.search.elastic_search_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticSearchCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elastic/search/elastic_search_crud/ElasticSearchCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastic.search.elastic_search_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
